873b32a8923ed970a6c058e21e9c4c2d1dc999bc
zedwarth <zedwarth@gmail.com>
2014-01-31 14:00:24 -0600

    Meditation on triangles.

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  case 
+  when a == b && a == c
+	  return :equilateral
+  when (a == b) || (a == c) || (b == c)
+	  return :isosceles
+  else
+	  return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
